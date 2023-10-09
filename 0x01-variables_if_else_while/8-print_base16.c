443ffd40a57dbb136b1b139c154643311e98c96b
Yvansoh <evansmulei99@gmail.com>
2023-10-09 13:54:22 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char hexChar;
+
+	for (hexChar = '0'; hexChar <= '9'; hexChar++)
+	{
+		putchar(hexChar);
+	}
+
+	for (hexChar = 'a'; hexChar <= 'f'; hexChar++)
+	{
+		putchar(hexChar);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+

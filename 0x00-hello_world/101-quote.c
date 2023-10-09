624638aa1757285538c7c3195c11fe1e596799b6
Yvansoh <evansmulei99@gmail.com>
2023-10-09 09:28:06 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,8 @@
 #include <unistd.h>
+/**
+ * main - A program that prints a code followed by a new line
+ * Return: 1 (indicating failure)
+ */
 
 int main(void) {
     write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);

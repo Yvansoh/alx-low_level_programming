63cd5212528b5ebacbde814d28002e19bc3cf300
Yvansoh <evansmulei99@gmail.com>
2023-10-09 09:39:14 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <unistd.h>
-
 /**
  * main - Entry point of the program
- *
- * Return: Always 1 (indicating failure)
+ * Return: Always 1.
  */
 int main(void)
 {
-	write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
 

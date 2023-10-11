32fe8d83c8f59dd2e6079a67f250d7ab08969b87
Yvansoh <evansmulei99@gmail.com>
2023-10-11 15:51:31 -0700

    Time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,35 +2,32 @@
 
 /**
  * jack_bauer - Print every minute
- * Return: ...
+ * Return: void
  */
-
 void jack_bauer(void)
+{
+    int a, b, c, d;
 
-{
+    for (a = 0; a < 24; a++) /* Loop through hours (00 to 23) */
+    {
+        for (b = 0; b < 60; b++) /* Loop through minutes (00 to 59) */
+        {
+            /* Calculate tens and ones digits of hour and minute */
+            c = a / 10;
+            d = a % 10;
 
-int a, b, c, d;
+            _putchar(c + '0');
+            _putchar(d + '0');
+            _putchar(':');
 
-for (a = 0; a <= 2; a++)
-{
-for (b = 0; b <= 9; b++)
-{
-if ((a <= 1 && b < 9) || (a <= 2 && b <= 3))
-{
-for (c = 0; c <= 5; c++)
-{
-for (d = 0; d <= 9; d++)
-{
-_putchar(a + '0');
-_putchar(b + '0');
-_putchar(58);
-_putchar(c + '0');
-_putchar(d + '0');
-_putchar('\n');
-}
-}
-}
-}
+            c = b / 10;
+            d = b % 10;
+
+            _putchar(c + '0');
+            _putchar(d + '0');
+
+            _putchar('\n');
+        }
+    }
 }
 
-}

be5724776035bdfb2595dab35ef6ecae7dfc6bff
Yvansoh <evansmulei99@gmail.com>
2023-10-12 14:04:46 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Adds twon integers
+ * @n1: first integer used
+ * @n2: second integer used
+ * Return: Integer value
+ */
+
+int add(int n1, int n2)
+
+{
+
+	return (n1 + n2);
+
+}

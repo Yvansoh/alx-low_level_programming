c80936e1f96ad1ea44654787e56c68d1295160ca
Yvansoh <evansmulei99@gmail.com>
2023-10-17 14:40:51 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,26 +6,26 @@
  */
 void puts_half(char *str)
 {
-	int length = 0;
-	int i;
+    int length = 0;
+    int i;
 
-	/* Calculate the length of the string */
-	while (str[length] != '\0')
-		length++;
+    /* Calculate the length of the string */
+    while (str[length] != '\0')
+        length++;
 
-	/* Determine the starting index for the second half */
-	if (length % 2 == 0)
-		i = length / 2; /* Even length: start at half index */
-	else
-		i = (length - 1) / 2; /* Odd length: start at (length - 1) / 2 index */
+    /* Determine the starting index for the second half */
+    if (length % 2 == 0)
+        i = length / 2; /* Even length: start at half index */
+    else
+        i = (length + 1) / 2; /* Odd length: start at (length + 1) / 2 index */
 
-	/* Print the second half of the string */
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
+    /* Print the second half of the string */
+    while (str[i] != '\0')
+    {
+        _putchar(str[i]);
+        i++;
+    }
 
-	_putchar('\n');
+    _putchar('\n');
 }
 

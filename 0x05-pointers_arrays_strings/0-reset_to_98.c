7f4230bab3651592cd8b57b3243092b2d84fd2f3
Yvansoh <evansmulei99@gmail.com>
2023-10-17 11:36:57 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - character that takes a point to an int
+ * and updates the value it points to to 98
+ * @n: character to be tested
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n);
+
+{
+	*n = 98
+}
+

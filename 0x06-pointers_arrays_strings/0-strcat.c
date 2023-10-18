86f9a6fccebb4e1e5baabafbe146f235dcecbe2d
Yvansoh <evansmulei99@gmail.com>
2023-10-18 09:24:17 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: string to be tested
+ * @src: string to be tested
+ * Return: 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+strcat(src, dest);
+}
+

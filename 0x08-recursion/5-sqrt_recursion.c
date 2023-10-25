f8afc0c97b1622cf3dfbfe58648c7843f0e1104d
Yvansoh <evansmulei99@gmail.com>
2023-10-25 14:32:20 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,10 +8,10 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	if (n == 0 || n == 1)
-		return (n);
+if (n < 0)
+return (-1);
+if (n == 0 || n == 1)
+return (n);
 
-	return (_sqrt_helper(n, 1));
+return (_sqrt_helper(n, 1));
 }

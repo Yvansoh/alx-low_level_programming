a812d327f94470d7bab47fe14887921d807e6f9a
Yvansoh <evansmulei99@gmail.com>
2023-10-25 10:18:35 -0700

    Printing length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Function that returns length of a string
+ * @s: character to be tested
+ * Return: Always 0
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	else
+
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}

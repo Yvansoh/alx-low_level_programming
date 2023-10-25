9cd6154c279b15239ee6af32d97151368d8fef7e
Yvansoh <evansmulei99@gmail.com>
2023-10-25 14:25:49 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - function that returns the natural sroot of a number
+ * @n: number to be tested
+ * Return: 0 if has natural squre root and -1 if it does not have
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+
+	return (_sqrt_recursion(n, 1));
+}

e21643a3a326cc7689a451540d6f9a6891f328c0
Yvansoh <evansmulei99@gmail.com>
2023-10-26 06:03:56 -0700

    Print square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,40 +1,36 @@
 #include "main.h"
 
+int actual_sqrt_recursion(int n, int i);
 /**
- * _sqrt_recursion - Finds the square root of a number
+ * _sqrt_recursion - Finds the natural square root of a number
  * @n: Integer to test
  * Return: Square root, -1 for a num with no square root
  */
 
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-	{
-		return (-1);
-	}
-	if (n == 0)
-	{
-		return (0);
-	}
-	return (actual_sqrt_recursion(n, 1));
+if (n < 0)
+{
+return (-1);
+}
+else
+{
+return (actual_sqrt_recursion(n, 0));
+}
 }
 
 /**
- * actual_sqrt_recursion - Recursively finds the integer square root of number
- * @n: Number to test
- * @i: Iterator
- * Return: Square root or -1 if there's no exact square root
- */
+* actual_sqrt_recursion - recursively finds natural sqrt of a number
+* @n: Number to test
+* @i: Iterator
+* Return: Square root
+*/
 
 int actual_sqrt_recursion(int n, int i)
 {
-	if (i * i == n)
-	{
-		return (i);
-	}
-	if (i * i > n)
-	{
-		return (-1);
-	}
-	return (actual_sqrt_recursion(n, i + 1));
+if (i * i > n)
+return (-1);
+if (i * i == n)
+return (i);
+return (actual_sqrt_recursion(n, i + 1));
 }

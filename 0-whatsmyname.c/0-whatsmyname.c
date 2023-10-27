5a6607c879e0a83f4a578776e13f0c7247f0e47c
Yvansoh <evansmulei99@gmail.com>
2023-10-27 05:01:56 -0700

    0-whatsmyname.c

---
+++ 0-whatsmyname.c/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * whatsmyname.c - Print program name
+ * main - entry point
+ * @argc: arguement count
+ * @argv: array of arguement values
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+
+return (0);
+}
---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * whatsmyname.c - prints program name
+ * main - entry point
+ * @argc: arguement count
+ * @argv: array of arguements
+ * Return: 0 if successful
+ */
+
+int main(int argc, char *argv[]);
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

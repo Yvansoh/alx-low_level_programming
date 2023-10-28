c27695c55a841d547b94af471c8a7379663e89fa
Yvansoh <evansmulei99@gmail.com>
2023-10-28 09:01:39 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -13,10 +13,12 @@ int main(int argc, char *argv[])
 	int i;
 
 	printf("Arguement (argc) is: %d\n", argc);
+
 	for (i = 0; i <argc; i++)
 	{
 		printf("%s\n", argv[i]);
-		printf("Arguements passed are: \n");
 	}
+	
 	return (0);
+	
 }

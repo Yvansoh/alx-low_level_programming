b74238d44a7ce61e8a6b5edf52dc97e9740c318a
Yvansoh <evansmulei99@gmail.com>
2023-10-28 08:22:15 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,6 +10,13 @@
 
 int main(int argc, char *argv[])
 {
-	printf("The program name is: %s\n", argv[0]);
+	int i;
+
+	printf("Arguement count is: %d\n", argc);
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[0]);
+		printf("The program name is: \n");
+	}
 	return (0);
 }

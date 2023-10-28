fa6ff1828b38d284198dba51fa425a1732756aa9
Yvansoh <evansmulei99@gmail.com>
2023-10-28 07:35:54 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,15 +10,8 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+	printf("%s\n", argv[0]);
+	printf("The arguements passesd are: \n");
 
-	printf("Arguement count (argc) is: %d\n", argc);
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[0]);
-		printf("The arguements passesd are: \n");
-	}
 	return (0);
-
 }

fc0b339120be2fe15e5955d19a0b63b1d2330ed4
Yvansoh <evansmulei99@gmail.com>
2023-10-28 08:41:16 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,16 +1,16 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
  * main - prints number of arguements passed to it
  * @argc: arguement count
  * @argv: array of arguements
+ *
  * Return: 0 if successful
  */
 
 int main(int argc, char *argv[])
 {
-	int num_args = argc;
+	int num_args = argc - 1;
 
 	printf("Number of arguements: %d\n", num_args);
 

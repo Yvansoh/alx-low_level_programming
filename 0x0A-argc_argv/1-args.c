1758206032014e72d3da4d975d9f720c9faad93e
Yvansoh <evansmulei99@gmail.com>
2023-10-28 09:20:40 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints number of arguements passed to it
+ * main - Entry point of program
  * @argc: arguement count
  * @argv: array of arguements
  *
@@ -11,7 +11,7 @@
 int main(int argc, char *argv[])
 {
 	printf("%d\n", argc);
-	print("\n");
+	printf("\n");
 
 	return (0);
 }

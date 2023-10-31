e020ae23b999a0317687cf7cbf2b08c730aacd87
Yvansoh <evansmulei99@gmail.com>
2023-10-31 14:21:22 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,32 +3,49 @@
 #include <stdlib.h>
 
 /**
- * _strdup - Returns a pointer but to a new allocated space(copy of original)
- * @str: char
+ * str_concat - Concatenates two strings
+ * @s1: String, Input one to be linked
+ * @s2: string, Input two to be linked
  *
- * Return: pointer to duplicated string, NULL if fails
+ * Return: Always 0 (Success), NULL if fails
  */
 
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
-char *sss;
-int a = 0;
-int z = 0;
+char *link;
+int b = 0;
+int ti = 0;
 
-if (str == NULL)
+if (s1 == NULL || s2 == NULL)
+{
+return ("");
+}
+
+b = ti = 0;
+
+while (s1[b] != '\0')
+b++;
+while (s2[ti] != '\0')
+ti++;
+
+link = malloc(sizeof(char) * (b + ti + 1));
+if (link == NULL)
 {
 return (NULL);
 }
-sss = malloc(sizeof(char) * (a + 1));
-if (sss == NULL)
+b = ti = 0;
+while (s1[b] != '\0')
 {
-return (NULL);
+link[b] = s1[b];
+b++;
 }
-while (str[a] != '\0')
-a++;
+while (s2[ti] != '\0')
+{
+link[ti] = s2[ti];
+ti++;
+b++;
+}
+link[b] = '\0';
 
-for (z = 0; str[z]; z++)
-sss[z] = str[z];
-
-return (sss);
+return (link);
 }

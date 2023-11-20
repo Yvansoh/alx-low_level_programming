6ca4d73f826b05fa1120d8da31bb638f0086424e
Yvansoh <evansmulei99@gmail.com>
2023-11-20 03:48:12 -0800

    Check for a digit through 0 to 9

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,12 +5,12 @@
  * @c: character to be checked
  * Return: 1 for uppercase character or 0 for anything else
  */
+
 int _isupper(int c);
 {
-if (c >= 65 && c <= 90)
-{
-return (1);
-}
-return (0);
+if (c >= '0' && c <= '9')
+	return (1);
+else
+	return (0);
 }
 

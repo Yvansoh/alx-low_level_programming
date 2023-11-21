f603f60852a3211a82a80541b2e026693f7fb8a7
Yvansoh <evansmulei99@gmail.com>
2023-11-21 15:54:56 -0800

    Free dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,66 +1,16 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include "dog.h"
 
 /**
- * free_dog - Function that frees dog
- * @d: pointer to dog structure
- *
- * Return:
- */
-
+* free_dog - frees memory allocated for a struct dog
+* @d: struct dog to free
+*/
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-	{
-		return;
-	}
-
-	if (d->name != NULL)
-	{
-		free(d->name);
-	}
-
-	if (d->owner != NULL)
-	{
-		free(d->owner);
-	}
-
-	free(d);
-}
-
-/**
- * new_dog - dog structure
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
- *
- * Return: new dog
- */
-
-dog_t *new_dog(char *name, float age, char *owner)
+if (d)
 {
-	dog_t *new_dog = malloc(sizeof(dog_t));
-
-	if (new_dog == NULL)
-	{
-		return (NULL);
-	}
-
-	new_dog->name = strdup(name);
-	if (new_dog->name == NULL)
-	{
-		free(new_dog);
-		return (NULL);
-	}
-
-	new_dog->age = age;
-	new_dog->owner = strdup(owner);
-
-	if (new_dog->owner == NULL)
-	{
-		free(new_dog->name);
-		free(new_dog);
-		return (NULL);
-	}
-	return (new_dog);
+free(d->name);
+free(d->owner);
+free(d);
+}
 }
